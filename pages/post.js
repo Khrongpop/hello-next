07f68217773aaef00b2c3265e19eaeb5df8c6aef
Khrongpop <khrongpop@192.168.1.48>
2019-03-23 10:15:29 +0700

    fetch data

--- pages/post.js
+++ pages/post.js
@@ -1,11 +1,31 @@
 import { withRouter } from 'next/router'
 import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
 
-const Page = withRouter(props => (
+// const Page = withRouter(props => (
+//   <Layout>
+//     <h1>{props.router.query.title}</h1>
+//     <p>This is the blog post content.</p>
+//   </Layout>
+// ))
+
+const Post = (props) => (
   <Layout>
-    <h1>{props.router.query.title}</h1>
-    <p>This is the blog post content.</p>
+    <h1>{props.res.name}</h1>
+    <p>{props.res.summary.replace(/<[/]?p>/g, '')}</p>
+    <img src={props.res.image.medium} />
   </Layout>
-))
+)
 
-export default Page
+Post.getInitialProps = async function(context) {
+  const { id } = context.query
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`).then(response => response.json())
+  // const show = await res.json()
+  
+
+  console.log(`Fetched show: ${res.name}`)
+
+  return { res }
+}
+
+export default Post
